4c32fedb3a5c541d84360db5d916fa1638759ab1
NikolaIliev1 <nikolailiew636@gmail.com>
2025-03-27 23:43:47 +0200

    add CSVParser

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,5 +1,6 @@
 import models.Table;
 import models.Cell;
+import parser.CSVParser;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,5 +13,14 @@ public class Main {
         System.out.println(cell.getValue());
         cell.setValue("World");
         System.out.println(cell.getValue());
+
+        String content = "1,2,3\n4,5,6\n7,8,9";
+        var parsedTable = CSVParser.parse(content);
+        for (var row : parsedTable) {
+            for (var cellValue : row) {
+                System.out.print(cellValue + " | ");
+            }
+            System.out.println();
         }
     }
+}
